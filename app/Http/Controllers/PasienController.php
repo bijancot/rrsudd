bdf282cdcf4110edbaece5286d6c1343b6bdc008
devisap <ilham.sagitaputra@gmail.com>
2020-09-12 09:25:19 +0700

    Read Data : Table Pasien

--- app/Http/Controllers/PasienController.php
+++ app/Http/Controllers/PasienController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\ResumeMedis;
 
 class PasienController extends Controller
 {
     public function index()
     {
-        return view('pasien.index');
+        $resumes=ResumeMedis::all();
+        return view('pasien.index', compact('resumes'));
     }
 }
--- resources/views/pasien/index.blade.php
+++ resources/views/pasien/index.blade.php
@@ -14,20 +14,55 @@
                 <table id="example" class="table table-striped table-bordered dt-responsive nowrap" style="width:100%">
                     <thead>
                         <tr>
-                            <th>First name</th>
-                            <th>Last name</th>
-                            <th>Position</th>
-                            <th>Office</th>
-                            <th>Age</th>
-                            <th>Start date</th>
-                            <th>Salary</th>
-                            <th>Extn.</th>
-                            <th>E-mail</th>
+                            <th>Diagnosa Masuk</th>
+                            <th>Anamnesis</th>
+                            <th>Pemeriksaan Fisik</th>
+                            <th>Pemeriksaan Pengunjung</th>
+                            <th>Obat Selama Rawat</th>
+                            <th>Diagnosa Akhir</th>
+                            <th>Tindakan Operasi</th>
+                            <th>Obat Terapi Pulang</th>
+                            <th>Pengobtan Lanjutan</th>
+                            <th>Tanggal Kontrol</th>
+                            <th>Kondisi Saat Pulang</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                        @foreach ($resumes as $resume)
+                            <tr>
+                                <td>{{$resume->diagnosa_masuk}}</td>
+                                <td>{{$resume->anamnesis}}</td>
+                                <td>{{$resume->pemeriksaan_fisik}}</td>
+                                <td>{{$resume->pemeriksaan_pengunjung}}</td>
+                                <td>{{$resume->obat_selama_rawat}}</td>
+                                <td>{{$resume->diagnosa_akhir}}</td>
+                                <td>{{$resume->tindakan_operasi}}</td>
+                                <td>{{$resume->obat_terapi_pulang}}</td>
+                                <td>
+                                    @if ($resume->pengobatan_lanjutan['poliklinik'] == "on")
+                                        Poliklinik&nbsp;
+                                    @endif
+                                    {{$resume->pengobatan_lanjutan['pengobatan_lanjutan']}}
+                                </td>
+                                <td>{{$resume->tgl_kontrol}}</td>
+                                <td>
+                                    @if (!empty($resume->kondisi_saat_pulang['ket_dirujuk']))
+                                        {{$resume->kondisi_saat_pulang['ket_dirujuk']." Alasan: "}}
+                                        @if ($resume->kondisi_saat_pulang['alasan'] == null)
+                                            -
+                                        @endif
+                                    @else
+                                        {{$resume->kondisi_saat_pulang}}
+                                    @endif
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-warning">Edit</button>
+                                    <button type="button" class="btn btn-danger">Hapus</button>
+                                </td>
+                            </tr>
+                        @endforeach
+                        {{-- <tr>
                             <td>Tiger</td>
                             <td>Nixon</td>
                             <td>System Architect</td>
@@ -36,6 +71,8 @@
                             <td>2011/04/25</td>
                             <td>$320,800</td>
                             <td>5421</td>
+                            <td>t.nixon@datatables.net</td>
+                            <td>t.nixon@datatables.net</td>
                             <td>t.nixon@datatables.net</td>
                             <td>
                                 <button type="button" class="btn btn-warning">Edit</button>
@@ -52,162 +89,13 @@
                             <td>$170,750</td>
                             <td>8422</td>
                             <td>g.winters@datatables.net</td>
+                            <td>g.winters@datatables.net</td>
+                            <td>g.winters@datatables.net</td>
                             <td>
                                 <button type="button" class="btn btn-warning">Edit</button>
                                 <button type="button" class="btn btn-danger">Hapus</button>
                             </td>
-                        </tr>
-                        <tr>
-                            <td>Ashton</td>
-                            <td>Cox</td>
-                            <td>Junior Technical Author</td>
-                            <td>San Francisco</td>
-                            <td>66</td>
-                            <td>2009/01/12</td>
-                            <td>$86,000</td>
-                            <td>1562</td>
-                            <td>a.cox@datatables.net</td>
-                            <td>
-                                <button type="button" class="btn btn-warning">Edit</button>
-                                <button type="button" class="btn btn-danger">Hapus</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Cedric</td>
-                            <td>Kelly</td>
-                            <td>Senior Javascript Developer</td>
-                            <td>Edinburgh</td>
-                            <td>22</td>
-                            <td>2012/03/29</td>
-                            <td>$433,060</td>
-                            <td>6224</td>
-                            <td>c.kelly@datatables.net</td>
-                            <td>
-                                <button type="button" class="btn btn-warning">Edit</button>
-                                <button type="button" class="btn btn-danger">Hapus</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Airi</td>
-                            <td>Satou</td>
-                            <td>Accountant</td>
-                            <td>Tokyo</td>
-                            <td>33</td>
-                            <td>2008/11/28</td>
-                            <td>$162,700</td>
-                            <td>5407</td>
-                            <td>a.satou@datatables.net</td>
-                            <td>
-                                <button type="button" class="btn btn-warning">Edit</button>
-                                <button type="button" class="btn btn-danger">Hapus</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Brielle</td>
-                            <td>Williamson</td>
-                            <td>Integration Specialist</td>
-                            <td>New York</td>
-                            <td>61</td>
-                            <td>2012/12/02</td>
-                            <td>$372,000</td>
-                            <td>4804</td>
-                            <td>b.williamson@datatables.net</td>
-                            <td>
-                                <button type="button" class="btn btn-warning">Edit</button>
-                                <button type="button" class="btn btn-danger">Hapus</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Herrod</td>
-                            <td>Chandler</td>
-                            <td>Sales Assistant</td>
-                            <td>San Francisco</td>
-                            <td>59</td>
-                            <td>2012/08/06</td>
-                            <td>$137,500</td>
-                            <td>9608</td>
-                            <td>h.chandler@datatables.net</td>
-                            <td>
-                                <button type="button" class="btn btn-warning">Edit</button>
-                                <button type="button" class="btn btn-danger">Hapus</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Rhona</td>
-                            <td>Davidson</td>
-                            <td>Integration Specialist</td>
-                            <td>Tokyo</td>
-                            <td>55</td>
-                            <td>2010/10/14</td>
-                            <td>$327,900</td>
-                            <td>6200</td>
-                            <td>r.davidson@datatables.net</td>
-                            <td>
-                                <button type="button" class="btn btn-warning">Edit</button>
-                                <button type="button" class="btn btn-danger">Hapus</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Colleen</td>
-                            <td>Hurst</td>
-                            <td>Javascript Developer</td>
-                            <td>San Francisco</td>
-                            <td>39</td>
-                            <td>2009/09/15</td>
-                            <td>$205,500</td>
-                            <td>2360</td>
-                            <td>c.hurst@datatables.net</td>
-                            <td>
-                                <button type="button" class="btn btn-warning">Edit</button>
-                                <button type="button" class="btn btn-danger">Hapus</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Sonya</td>
-                            <td>Frost</td>
-                            <td>Software Engineer</td>
-                            <td>Edinburgh</td>
-                            <td>23</td>
-                            <td>2008/12/13</td>
-                            <td>$103,600</td>
-                            <td>1667</td>
-                            <td>s.frost@datatables.net</td>
-                            <td>
-                                <button type="button" class="btn btn-warning">Edit</button>
-                                <button type="button" class="btn btn-danger">Hapus</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Jena</td>
-                            <td>Gaines</td>
-                            <td>Office Manager</td>
-                            <td>London</td>
-                            <td>30</td>
-                            <td>2008/12/19</td>
-                            <td>$90,560</td>
-                            <td>3814</td>
-                            <td>j.gaines@datatables.net</td>
-                            <td>
-                                <button type="button" class="btn btn-warning">Edit</button>
-                                <button type="button" class="btn btn-danger">Hapus</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Quinn</td>
-                            <td>Flynn</td>
-                            <td>Support Lead</td>
-                            <td>Edinburgh</td>
-                            <td>22</td>
-                            <td>2013/03/03</td>
-                            <td>$342,000</td>
-                            <td>9497</td>
-                            <td>q.flynn@datatables.net</td>
-                            <td>
-                                <button type="button" class="btn btn-warning">Edit</button>
-                                <button type="button" class="btn btn-danger">Hapus</button>
-                            </td>
-                        </tr>
-                        
+                        </tr> --}}
                     </tbody>
                 </table>
 
