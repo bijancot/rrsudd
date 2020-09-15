50a0eed2c9b860ff0a7eb3eff6c01d85135a1990
Bima Reynaldi Sumitro <50427761+XxxxReynaldi@users.noreply.github.com>
2020-09-15 15:33:24 +0700

    tes update branch

--- app/Http/Controllers/ResumeMedisController.php
+++ app/Http/Controllers/ResumeMedisController.php
@@ -7,11 +7,13 @@ use App\ResumeMedis;
 
 class ResumeMedisController extends Controller
 {
-    public function FormInsert(){
+    public function FormInsert()
+    {
         return view('ResumeMedis/form-insert');
     }
 
-    public function Insert(Request $req){
+    public function Insert(Request $req)
+    {
         $resume = new ResumeMedis();
         $resume->diagnosa_masuk = $req->get('diagnosa_masuk');
         $resume->anamnesis = $req->get('anamnesis');
@@ -25,37 +27,39 @@ class ResumeMedisController extends Controller
         $resume->pengobatan_lanjutan = $req->get('pengobatan_lanjutan');
         $resume->tgl_kontrol = $req->get('tgl_kontrol');
         $resume->status = "0";
-        
+
         $resume->pengobatan_lanjutan = [
             'poliklinik' => $req->get('poliklinik'),
             'pengobatan_lanjutan' => $req->get('pengobatan_lanjutan')
         ];
-        
-        if($req->get('kondisi_saat_pulang') == "dirujuk"){
+
+        if ($req->get('kondisi_saat_pulang') == "dirujuk") {
             $resume->kondisi_saat_pulang = [
                 'ket_dirujuk' => $req->get('ket_dirujuk'),
                 'alasan' => $req->get('alasan')
             ];
-        }else{
+        } else {
             $resume->kondisi_saat_pulang = $req->get('kondisi_saat_pulang');
         }
         $resume->save();
-        
+
         return redirect('/pasien');
     }
-    
-    public function FormEdit($id){
-        $resume =ResumeMedis::find($id);
+
+    public function FormEdit($id)
+    {
+        $resume = ResumeMedis::find($id);
         return view('/ResumeMedis/form-update', compact('resume'));
     }
 
-    public function Update(Request $req, $id){
-        if($req->status_update == "1"){
+    public function Update(Request $req, $id)
+    {
+        if ($req->status_update == "1") {
             //Edit Status
             $resume = ResumeMedis::find($id);
             $resume->status = "1";
             $resume->save();
-    
+
             //Insert New Data
             $resume = new ResumeMedis();
             $resume->diagnosa_masuk = $req->get('diagnosa_masuk');
@@ -70,29 +74,30 @@ class ResumeMedisController extends Controller
             $resume->pengobatan_lanjutan = $req->get('pengobatan_lanjutan');
             $resume->tgl_kontrol = $req->get('tgl_kontrol');
             $resume->status = "0";
-            
+
             $resume->pengobatan_lanjutan = [
                 'poliklinik' => $req->get('poliklinik'),
                 'pengobatan_lanjutan' => $req->get('pengobatan_lanjutan')
             ];
-            
-            if($req->get('kondisi_saat_pulang') == "dirujuk"){
+
+            if ($req->get('kondisi_saat_pulang') == "dirujuk") {
                 $resume->kondisi_saat_pulang = [
                     'ket_dirujuk' => $req->get('ket_dirujuk'),
                     'alasan' => $req->get('alasan')
                 ];
-            }else{
+            } else {
                 $resume->kondisi_saat_pulang = $req->get('kondisi_saat_pulang');
             }
             $resume->save();
             return redirect('/pasien');
-        }else{
+        } else {
             return redirect('/pasien');
         }
     }
-    public function Delete(Request $req){
+    public function Delete(Request $req)
+    {
         $resume = ResumeMedis::find($req->id);
-        $resume->status = "2";
+        $resume->status = "3";
         $resume->save();
         return redirect('/pasien');
     }
